f8f0f2e750e3f5f5d26b70024691c766a41b0529
sherzod <azamovshavkat98@gmail.com>
2024-02-20 17:11:58 +0500

    desc

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,16 +4,17 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        await super().connect()
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = f"chat_{self.room_name}"
 
+        # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
         await self.accept()
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
