25cbd150ace461bae00f6b53fe42568054a0334d
Imom Qurbonov <imomqurbonov20@gmail.com>
2024-02-22 21:17:48 +0500

    Discount product

--- customer/urls.py
+++ customer/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from customer.views import DiscountProductAPIView, ShippignAddressAPIView
+from customer.views import (
+    DiscountProductAPIView,
+    ShippignAddressAPIView,
+    MyFavouriteAPIView,
+    DiscountProductListAPIView,
+    DiscountCategoryListAPIView
+)
 
 urlpatterns = [
+    path('Favourites/', MyFavouriteAPIView.as_view(), name='favourites'),
     path('shipping_address/', ShippignAddressAPIView.as_view(), name='shipping-address'),
-    path('discount_product/', DiscountProductAPIView.as_view(), name='discount-product'),
+    path('discount_product/<int:category_id>', DiscountProductAPIView.as_view(), name='discount-product'),
+    path('discount_product_list/', DiscountProductListAPIView.as_view(), name='discount-product-list'),
+    path('discount_category_list/', DiscountCategoryListAPIView.as_view(), name='discount-category-list')
 ]
